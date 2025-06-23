a3b6ce2bb731011105c77321d37c1b2fc9eadfd8
Ayush Raj <122692756+ayush09062004@users.noreply.github.com>
2025-06-23 15:48:41 +0200

    Added RCSpace Class

--- mozaik/sheets/population_selector.py
+++ mozaik/sheets/population_selector.py
@@ -143,6 +143,42 @@ class RCGrid(PopulationSelector):
           logger.info("RCGrid> picked neurons: " + str(picked))
           return list(set(picked))
 
+class RCSpace(PopulationSelector):
+      """
+      This PopulationSelector selects all neurons in the sheet within a given size.
+      
+      Other parameters
+      ----------------
+
+      radius : float (degrees of visual field)
+
+      offset_x : float (the x axis offset from the center of the sheet, micro meters)
+      
+      offset_y : float (the y axis offset from the center of the sheet, micro meters)
+
+      """
+      
+      required_parameters = ParameterSet({
+        'radius' : float, # the radius of cortical space to select (degrees of visual field)
+        'offset_x' : float, # the x axis offset from the center of the sheet (micro meters)
+        'offset_y' : float, # the y axis offset from the center of the sheet (micro meters)
+      })  
+      
+      def generate_idd_list_of_neurons(self):
+          picked = []
+          z = self.sheet.pop.all_cells.astype(int) # PyNN population ids
+          o = numpy.array( (self.parameters.offset_x, self.parameters.offset_y, 0.0) )
+          for idd in z:
+            i = self.sheet.pop.id_to_index(idd)
+            a = numpy.array( (self.sheet.pop.positions[0][i], self.sheet.pop.positions[1][i], 0.0) )
+            if numpy.linalg.norm(a - o) < self.parameters.radius:
+              picked.append(idd)
+          print "population_selector : RCSpace : Total IDs:",len(z)
+          print "population_selector : RCSpace : Chosen IDs:",len(picked)
+          return list(set(picked))
+
+
+
 class RCGridDegree(PopulationSelector):
       """
       Select neurons on a grid which coordinates are in degree of visual space.
